cd79cd1a0093f9a5e26f802e5d969874a936654a
Raul Hernandez <optimusgamer55hdz@gmail.com>
2026-04-24 16:46:36 -0600

    refactor: logic problem in last bucle and change condition in do while's

--- 04_footballgames_practice/main.c
+++ 04_footballgames_practice/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h> 
-
+#include <stdbool.h>
 #define MAX_GAMES 10
 #define GOALS_POINTS 10
 #define ASSISTANTS_POINTS 5
@@ -13,12 +13,18 @@ int main(void) {
 	int assistants = 0;
 	int saves = 0;
 	int counter = 0;
+	int temp_scores = 0;
+	int total_scores_sum = 0;
 	
 	int max_scores_games [MAX_GAMES];
 
 	do {
 		printf("Insert quantity of plays in the week\n");
 		scanf("%d", &plays_of_week);
+
+		if(plays_of_week <= 0) {
+			printf("Invalid value please insert a valid value\n");
+		}
 	} while (plays_of_week <= 0 || plays_of_week > MAX_GAMES);
 
 	for (counter = 0; counter < plays_of_week; counter++) {
@@ -31,7 +37,7 @@ int main(void) {
 				printf("The value entered is invalid \n");
 				printf("please try again insert correct values\n");
 			}
-		} while (goals <= 0);
+		} while (goals < 0);
 
 		do {
 			printf("Insert the assistants in game\n");
@@ -41,7 +47,7 @@ int main(void) {
 				printf("The value entered is invalid\n");
 				printf("please try again insert correct values\n");
 			}
-		} while (assistants <= 0);
+		} while (assistants < 0);
 
 		do {
 			printf("Insert the saves in game\n");
@@ -51,11 +57,23 @@ int main(void) {
 				printf("The value entered is invalid\n");
 				printf("please try again insert correct values\n");
 			}
-		} while (saves <= 0);
+		} while (saves < 0);
 
+		temp_scores = points_of_game(goals, assistants, saves);
 
+		max_scores_games[counter] = temp_scores;
 
 	}
+
+	for (counter = 0; counter < plays_of_week; counter++) {
+
+		total_scores_sum = max_scores_games[counter];
+		printf("Total Scores: %d\n", total_scores_sum);
+	}
+
+
+
+
 	return 0;
 
 }
@@ -63,8 +81,8 @@ int main(void) {
 
 int points_of_game (int goals, int assistants, int saves){
 
-	int total_score = (goals * GOALS_POINTS + assistants * ASSSISTANTS_POINTS,
-			saves * SAVES_POINTS);
+	int total_score = ((goals * GOALS_POINTS) + (assistants * ASSISTANTS_POINTS) + 
+			(saves * SAVES_POINTS));
 
 	return total_score;
 } 
